5fb964c1c2529943cd1c5bb9fd945ef112aef4d6
Justyna Sienkiewicz <justyna.sienkiewicz@lstsoft.pl>
2024-03-27 18:45:30 +0100

    Is logged in

--- ShooterLink.Web/src/redux/auth/selectors.js
+++ ShooterLink.Web/src/redux/auth/selectors.js
@@ -1,5 +1,6 @@
-export const selectUserData = state => state.auth.user
-export const selectIsLoggedIn = state => state.auth.isLoggedIn
-export const selectIsLoading = state => state.auth.isLoading
-export const selectError = state => state.auth.error
-export const selectIsRegisteredSuccessfully = state => state.auth.isRegisterProcessSuccessful
+export const selectUserData = (state) => state.auth.user;
+export const selectIsLoggedIn = (state) => state.auth.token != null;
+export const selectIsLoading = (state) => state.auth.isLoading;
+export const selectError = (state) => state.auth.error;
+export const selectIsRegisteredSuccessfully = (state) =>
+  state.auth.isRegisterProcessSuccessful;
--- ShooterLink.Web/src/redux/auth/slice.js
+++ ShooterLink.Web/src/redux/auth/slice.js
@@ -1,66 +1,62 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { register } from './operations'
-import { logIn } from './operations'
-import { logOut } from './operations'
+import { createSlice } from "@reduxjs/toolkit";
+import { register } from "./operations";
+import { logIn } from "./operations";
+import { logOut } from "./operations";
 
-const handlePending = state => {
-	state.isLoading = true
-}
+const handlePending = (state) => {
+  state.isLoading = true;
+};
 
 const handleRejected = (state, action) => {
-	state.isLoading = false
-	state.error = action.payload
-}
+  state.isLoading = false;
+  state.error = action.payload;
+};
 
-const isPendingAction = action => {
-	return action.type.endsWith('/pending')
-}
+const isPendingAction = (action) => {
+  return action.type.endsWith("/pending");
+};
 
-const isRejectAction = action => {
-	return action.type.endsWith('/rejected')
-}
+const isRejectAction = (action) => {
+  return action.type.endsWith("/rejected");
+};
 
 const slice = createSlice({
-	name: 'auth',
-	initialState: {
-		user: { firstName: null, lastName: null, roles: null },
-		token: null,
-		isLoggedIn: false,
-		isLoading: false,
-		error: null,
-		isRegisterProcessSuccessful: false,
-	},
-	extraReducers: builder => {
-		builder
-			.addCase(register.fulfilled, state => {
-				state.user = null
-				state.token = null
-				state.isLoggedIn = false
-				state.isLoading = false
-				state.error = null
-				state.isRegisterProcessSuccessful = true
-			})
-			.addCase(logIn.fulfilled, (state, action) => {
-				state.user = {
-					firstName: action.payload.firstName,
-					lastName: action.payload.lastName,
-					roles: action.payload.roles,
-				}
-				state.token = action.payload.token
-				state.isLoggedIn = true
-				state.isLoading = false
-				state.error = null
-			})
-			.addCase(logOut().type, (state, action) => {
-				state.user = null
-				state.token = null
-				state.isLoggedIn = false
-				state.isLoading = false
-				state.error = null
-			})
-			.addMatcher(isPendingAction, handlePending)
-			.addMatcher(isRejectAction, handleRejected)
-	},
-})
+  name: "auth",
+  initialState: {
+    user: { firstName: null, lastName: null, roles: null },
+    token: null,
+    isLoading: false,
+    error: null,
+    isRegisterProcessSuccessful: false,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.fulfilled, (state) => {
+        state.user = null;
+        state.token = null;
+        state.isLoading = false;
+        state.error = null;
+        state.isRegisterProcessSuccessful = true;
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.user = {
+          firstName: action.payload.firstName,
+          lastName: action.payload.lastName,
+          roles: action.payload.roles,
+        };
+        state.token = action.payload.token;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(logOut().type, (state, action) => {
+        state.user = null;
+        state.token = null;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addMatcher(isPendingAction, handlePending)
+      .addMatcher(isRejectAction, handleRejected);
+  },
+});
 
-export const authReducer = slice.reducer
+export const authReducer = slice.reducer;
